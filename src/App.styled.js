61896e6656b1675feb31754d20b0d840756ced81
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-05-24 17:08:22 +0300

    Summary

--- src/App.styled.js
+++ src/App.styled.js
@@ -20,7 +20,7 @@ export const AppLogo = styled.img`
 `
 
 export const AppHeader = styled.header`
-  background-color: purple;
+  background-color: whitesmoke;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
