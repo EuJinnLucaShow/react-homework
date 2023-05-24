37bbd2b873152a846573ff6188ea7f172552bd9e
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-05-24 12:19:46 +0300

    Update App.styled.js

--- src/App.styled.js
+++ src/App.styled.js
@@ -20,14 +20,14 @@ export const AppLogo = styled.img`
 `
 
 export const AppHeader = styled.header`
-  background-color: #5e2b58;
+  background-color: white;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   font-size: calc(10px + 2vmin);
-  color: white;
+  color: black;
 `
 
 export const AppLink = styled.a`
